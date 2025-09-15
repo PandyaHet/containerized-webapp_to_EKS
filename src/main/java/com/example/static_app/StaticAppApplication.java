e13d1edf30ddde41661fba16e320110ae4899ccb
Het Pandya <51289816+PandyaHet@users.noreply.github.com>
2025-09-15 11:57:03 -0400

    Second Commit

---
+++ src/main/java/com/example/static_app/StaticAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.static_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/static_app/StaticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.static_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
